e3c890ba8430a3161bc11eb4d6efd5ffa2864b07
Sunny Siddhu <119279854+sunnysiddhu21@users.noreply.github.com>
2024-01-26 06:25:05 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treated as newer version of JS
+
+//alert(3+3) // we are using node.js not browser that'swhy it's throwing err
+
+console.log(3
+    +3);  // code readability should be high not like this
+
+    // tc39 and mdn documentation for any query in js
+
+let name="Sunny Siddhu"
+let age=22
+let isLoggedIn=false
+
+/* 
+   Datatypes
+   Primitive data types
+      numbers => 2 ^ 53
+      string
+      bigint
+      boolean - true/false
+      null => standalone value   ex - let name=null  - means- empty
+      undefined => variable initialized but value is not assigned
+      symbol => unique
+
+    object datatypes
+*/
+
+console.log(typeof null);
