1082aa0443ec07cd91f0e142a43f85ec304afdd7
Sunny Siddhu <119279854+sunnysiddhu21@users.noreply.github.com>
2024-01-27 07:15:03 +0000

    operations done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -27,5 +27,45 @@ let booleanLoggedIn=Boolean(isLoggedIn)
 let someNumber=33
 
 let StringNum=String(someNumber)
-console.log(StringNum);
-console.log(typeof(StringNum));
+// console.log(StringNum);
+// console.log(typeof(StringNum));
+
+// ******************** Operations *************
+let value=10
+let negValue=-value
+// console.log(negValue);
+
+/*   basic maths operations
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2); 
+
+*/
+
+let str1="Sunny"
+let str2=" Siddhu"
+let str3=str1+str2
+// console.log(str3);
+
+// complex situations in operations
+console.log("1"+2);  // output > 12
+console.log(1+"2");  // 12
+console.log("1"+"2"); // 12
+console.log("1"+2+2); // 122 
+// here js is doing all conversion in string type
+
+// but when we just reverse this, output will be different
+
+// console.log(1+2+"2");
+// here js is first adding interger and then string concatenate
+
+// Increament & decremnent
+// Read more on mdn documentions
+let cnt=100
+++cnt;
+console.log(cnt);
+cnt++;
+console.log(cnt);
