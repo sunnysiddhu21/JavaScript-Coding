ecac048cbeb86f73a7eeb821487abd33bdb63841
Sunny Siddhu <119279854+sunnysiddhu21@users.noreply.github.com>
2024-01-27 06:46:36 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+let score="33"
+// console.log(typeof score)
+//console.log(typeof(score));
+
+let valueInNumber=Number(score)
+// console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
+
+/*
+  "33" => 33
+  "33abc" => NaN  but NaN type is number
+  true/false = 1/0
+
+*/
+
+let isLoggedIn=1
+let booleanLoggedIn=Boolean(isLoggedIn)
+
+// console.log(booleanLoggedIn);
+
+/**
+  when 1=> true 0=> false
+  "" => false 
+  "sunny" => true
+ */
+
+let someNumber=33
+
+let StringNum=String(someNumber)
+console.log(StringNum);
+console.log(typeof(StringNum));
